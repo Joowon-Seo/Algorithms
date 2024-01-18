9c876bfb4e855aa17ff07282957fcd694a1fdacf
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2024-01-18 13:24:27 +0900

    [level 3] Title: 등굣길, Time: 1.03 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -3,23 +3,21 @@ class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] map = new int[n + 1][m + 1];
 		boolean[][] data = new boolean[n + 1][m + 1];
-//		int[] factorial = new int[m + n - 1];
-//
-//		if (puddles.length == 0) {
-//			factorial[0] = 1;
-//			factorial[1] = 1;
-//			for (int i = 2; i <= m + n - 2; i++) {
-//				factorial[i] = factorial[i - 1] * i % 1000000007;
-//			}
-//
-//			return factorial[m + n - 2] / (factorial[m - 1] * factorial[n - 1]) % 1000000007;
-//		}
+		int[] factorial = new int[m + n - 1];
 
-		if (puddles[0].length > 0) {
-			for (int i = 0; i < puddles.length; i++) {
+		if (puddles.length == 0) {
+			factorial[0] = 1;
+			factorial[1] = 1;
+			for (int i = 2; i <= m + n - 2; i++) {
+				factorial[i] = factorial[i - 1] * i % 1000000007;
+			}
+
+			return factorial[m + n - 2] / (factorial[m - 1] * factorial[n - 1]) % 1000000007;
+		}
+
+		for (int i = 0; i < puddles.length; i++) {
 				data[puddles[i][1]][puddles[i][0]] = true;
 			}
-		}
 
 		map[1][1] = 1;
 
